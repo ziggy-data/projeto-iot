a125c3eb50b453dcfed2f346ecfec8c3445ca27b
Reinaldo A Simoes <54004680+ziggy-data@users.noreply.github.com>
2022-11-06 22:11:12 -0300

    Update main.py

--- sender/main.py
+++ sender/main.py
@@ -9,7 +9,8 @@ MQTT_USUARIO   = ''
 MQTT_SENHA     = ''
 MQTT_TOPICO    = 'presenca'
 
-sensor = Pin(4, Pin.IN)
+# Configura a pinagem dos itens utilizados no código
+sensor = Pin(4, Pin.IN) 
 led = Pin(13, Pin.OUT)
 
 print("conectando ao WIFI", end="")
@@ -31,10 +32,13 @@ cliente.connect()
 print("Conectado ao servidor")
 
 while True:
+    # Vai verificar se o sensor de presença 'percebeu' alguém no ambiente e
+    # imprime uma mensagem e envia a mensagem para acender um led (com o dispositivo 'receiver')
     if sensor.value() == 1:
         print("Welcome home, Master!")
         led.on()
-        cliente.publish(MQTT_TOPICO, b'on')
+        #Envia em bytes a mensagem "on" para o dispositivo "receiver" receber a informação para alterar 
+        cliente.publish(MQTT_TOPICO, b'on') 
         time.sleep(3)
         led.off()
         cliente.publish(MQTT_TOPICO, b'off')
